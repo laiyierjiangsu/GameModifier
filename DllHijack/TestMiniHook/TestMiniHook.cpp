86498688cc8e994ca68c06382961f8a1fa297434
kaka <laiyierjiangsu@gmail.com>
2017-06-11 16:24:31 +0800

    Make testHook compile successfully

--- DllHijack/TestMiniHook/TestMiniHook.cpp
+++ DllHijack/TestMiniHook/TestMiniHook.cpp
@@ -2,10 +2,16 @@
 //
 
 #include "stdafx.h"
-
-
+#include "MiniHook/include/MinHook.h"
+#include <assert.h>
 int _tmain(int argc, _TCHAR* argv[])
 {
+
+	MH_STATUS hr = MH_Initialize();
+	assert(hr == MH_OK);
+
+	hr = MH_Uninitialize();
+	assert(hr == MH_OK);
 	return 0;
 }
 
